81dfa7ebb4099ea525ad928b23b572e717c201c0
AdrianScharff <adrian18juarez@gmail.com>
2023-07-06 15:00:27 -0600

    fix stylelint and eslint errors

--- JavaScript/about.js
+++ JavaScript/about.js
@@ -3,14 +3,14 @@ const hamburger = document.querySelector('.hamburgerMenu');
 const menu = document.querySelector('#menuOptions');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+  hamburger.classList.toggle('active');
 });
 
 const menuOptionsLinks = document.querySelectorAll('.menuOptionsLinks');
 menuOptionsLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-        menu.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    })
+  link.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  });
 });
